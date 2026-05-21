f96d0311fdc23e33ea70190a07ef7fd03450336d
Jhnvncntx <jhnvncntx@gmail.com>
2026-05-21 23:29:12 +0800

    Added header contents

--- labtrack-web/src/components/Sidebar.jsx
+++ labtrack-web/src/components/Sidebar.jsx
@@ -1,9 +1,29 @@
 import "./Sidebar.css";
+import bannerImage from "../assets/LabTrackBanner2.png";
 
 export function Sidebar() {
   return (
     <>
-      <div className="sidebar-container">THIS IS THE SIDEBAR</div>
+      <div className="sidebar-container">
+        <div className="sidebar-heading-container">
+          <img
+            className="header-banner"
+            scr={bannerImage}
+            alt="This is the banner."
+          />
+        </div>
+
+        <div className="sidebar-navs-container">
+          <div>Dashboard</div>
+          <div>Equipments</div>
+          <div>Records</div>
+          <div>Users</div>
+        </div>
+
+        <div className="sidebar-footing-container">
+          This is the sidebar footer.
+        </div>
+      </div>
     </>
   );
 }
